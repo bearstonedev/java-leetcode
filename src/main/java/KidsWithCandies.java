d9572a48f26080b683cfa8a06c59010cbfb4cf94
camcamca <23409643+camcamca@users.noreply.github.com>
2021-02-16 14:26:47 -0700

    Removed explicit package declaration from Boolean type in KidsWithCandies class.

--- src/main/java/KidsWithCandies.java
+++ src/main/java/KidsWithCandies.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class KidsWithCandies {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<java.lang.Boolean> output = new ArrayList<>(candies.length);
+        List<Boolean> output = new ArrayList<>(candies.length);
 
         int highest = 0;
         for (int i = 0; i < candies.length; i++) {
